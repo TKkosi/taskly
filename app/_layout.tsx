1afcc1b0d936f484329241984828f3442f7a71a0
Ekwuno kosi <kosisochukwu13@yahoo.com>
2025-08-10 23:30:32 +0100

    tab button

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,7 +8,7 @@ export default function Layout() {
   return (
     <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorCerulean }}>
       <Tabs.Screen name="index" options={{ title: "Shopping List" , tabBarIcon: ({ color ,size }) => <Feather name="list" size={24} color={color} />}} />
-      <Tabs.Screen name="counter" options={{ title: "Counter" , tabBarIcon: ({ color ,size }) => <FontAwesome5 name="plus" size={24} color={color} />}} />
+      <Tabs.Screen name="counter" options={{ title: "Counter" ,headerShown:false , tabBarIcon: ({ color ,size }) => <FontAwesome5 name="plus" size={24} color={color} />}} />
       <Tabs.Screen name="idea" options={{ title: "Idea" , tabBarIcon: ({ color ,size }) => <AntDesign name="bulb1" size={24} color={color} />}} />
     </Tabs>
   );
