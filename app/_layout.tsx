8153179a521beef98b216a4e1b85a55740cae9c1
Ekwuno kosi <kosisochukwu13@yahoo.com>
2025-08-10 23:00:20 +0100

    tab button

---
+++ app/_layout.tsx
@@ -0,0 +1,15 @@
+import { Tabs } from "expo-router";
+import { Feather } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { theme } from "@/theme";
+
+export default function Layout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorCerulean }}>
+      <Tabs.Screen name="index" options={{ title: "Shopping List" , tabBarIcon: ({ color ,size }) => <Feather name="list" size={24} color={color} />}} />
+      <Tabs.Screen name="counter" options={{ title: "Counter" , tabBarIcon: ({ color ,size }) => <FontAwesome5 name="plus" size={24} color={color} />}} />
+      <Tabs.Screen name="idea" options={{ title: "Idea" , tabBarIcon: ({ color ,size }) => <AntDesign name="bulb1" size={24} color={color} />}} />
+    </Tabs>
+  );
+}
